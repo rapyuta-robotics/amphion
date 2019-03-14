2a657900de9fe0fe26de1c99c0b900294cf79589
Nishanth Mane <nishanth.mane@rapyuta-robotics.com>
2019-03-14 18:05:46 +0530

    Setup Arrow display type - dimensions are matched with that of rviz

--- src/core/Arrow.js
+++ src/core/Arrow.js
@@ -1,11 +1,70 @@
-import Core from "./Index";
-import {MESSAGE_TYPE_VECTOR3STAMPED} from "../index";
+import Core from './Index';
 
-class Arrow extends Core {
-  constructor(ros, topicName) {
-    super(ros, topicName, MESSAGE_TYPE_VECTOR3STAMPED);
-    this.object = new THREE.Group();
+const defaultConfig = {
+  cone: {
+    radius: 1,
+    height: 0.25
+  },
+  cylinder: {
+    radius: 0.5,
+    height: 0.75
   }
+};
+
+class Arrow {
+  constructor(scene) {
+    this.scene = scene;
+    this.object = new THREE.Group();
+    this.scene.add(this.object);
+
+    this.init();
+  }
+
+  init() {
+    const { cone, cylinder } = defaultConfig;
+
+    // cone setup
+    const geometry = new THREE.ConeBufferGeometry(cone.radius, cone.height, 16);
+    const material = new THREE.MeshStandardMaterial({ color: 0xf0ff00 });
+    this.coneMesh = new THREE.Mesh(geometry, material);
+    this.coneMesh.position.y = -0.125;
+    this.coneMesh.castShadow = true;
+    this.coneMesh.receiveShadow = true;
+    this.object.add(this.coneMesh);
+
+    // Cylinder setup
+    const cyGeometry = new THREE.CylinderGeometry(
+      cylinder.radius, cylinder.radius, cylinder.height, 16
+    );
+    const cyMaterial = new THREE.MeshStandardMaterial({ color: 0xf0ff00 });
+    this.cylinderMesh = new THREE.Mesh(cyGeometry, cyMaterial);
+    this.cylinderMesh.position.y -= 2 * cone.height;
+    this.cylinderMesh.castShadow = true;
+    this.cylinderMesh.receiveShadow = true;
+
+    this.coneMesh.add(this.cylinderMesh);
+    this.object.add(this.coneMesh);
+  }
+
+  update(message) {
+    const { x: posX, y: posY, z: posZ } = message.pose.position;
+    const { x: scaleX, y: scaleY, z: scaleZ } = message.scale;
+
+    this.object.position.set(posX, posY, posZ);
+    this.object.scale.set(scaleX, scaleY, scaleZ);
+
+    this.coneMesh.material.color = new THREE.Color(
+      message.color.r, message.color.g, message.color.b
+    );
+
+    this.cylinderMesh.material.color = new THREE.Color(
+      message.color.r, message.color.g, message.color.b
+    );
+  }
+
+  // Config(config) {
+
+  // }
 }
 
 export default Arrow;
