6b63cc70de59a75463f999cc236d20c99f16f821
Nishanth Mane <nishanth.mane@rapyuta-robotics.com>
2019-04-23 16:32:19 +0530

    added odo-options - replace all the objects in the object pool with the selected type - config the objects in pool for every updated options received.

--- src/viz/DisplayOdometry.js
+++ src/viz/DisplayOdometry.js
@@ -4,23 +4,21 @@ import { MESSAGE_TYPE_ODOMETRY } from '../utils/constants';
 import Arrow from '../primitives/Arrow';
 import Group from '../primitives/Group';
 import * as TransformUtils from '../utils/transform';
-import { HEAD_LENGTH, HEAD_RADIUS, SHAFT_LENGTH, SHAFT_RADIUS } from './Pose';
+import { HEAD_LENGTH, HEAD_RADIUS, POSE_VIZ_TYPES, SHAFT_LENGTH, SHAFT_RADIUS } from './Pose';
+import Axes from '../primitives/Axes';
 
 const { THREE } = window;
 
 class DisplayOdometry extends Core {
-  constructor(ros, topicName, controlledObject) {
+  constructor(ros, topicName, options) {
     super(ros, topicName, MESSAGE_TYPE_ODOMETRY);
 
     this.object = null;
     this.objectPool = [];
     this.keepSize = 100;
     this.currentObject = -1;
-    this.tolerance = {
-      position: 0.1,
-      angle: 0.1,
-    };
-    this.setVizType(controlledObject);
+    this.options = options || {};
+    this.setVizType(options.controlledObject);
   }
 
   setVizType(controlledObject) {
@@ -35,7 +33,6 @@ class DisplayOdometry extends Core {
     let newKeepList = [];
 
     if (size === 0) {
-      this.removeAllObjects();
       this.keepSize = 0;
       return;
     }
@@ -65,19 +62,106 @@ class DisplayOdometry extends Core {
   }
 
   checkTolerance(position, rotation) {
+    const { positionTolerance, angleTolerance } = this.options;
     if (this.objectPool.length === 0) {
       return false;
     }
 
-    const positionTolerance = this.objectPool[this.currentObject]
-      .position.distanceTo(position) < this.tolerance.position;
-    const angleTolerance = this.objectPool[this.currentObject]
-      .quaternion.angleTo(rotation) < this.tolerance.angle;
-    if (positionTolerance && angleTolerance) {
+    const positionToleranceBool = this.objectPool[this.currentObject]
+      .position.distanceTo(position) < positionTolerance;
+    const angleToleranceBool = this.objectPool[this.currentObject]
+      .quaternion.angleTo(rotation) < angleTolerance;
+    if (positionToleranceBool && angleToleranceBool) {
       return true;
     }
 
     return false;
+  }
+
+  getObject() {
+    const { type } = this.options;
+    switch (type) {
+      case POSE_VIZ_TYPES.arrow:
+        return new Arrow();
+      case POSE_VIZ_TYPES.axes:
+        return new Axes();
+    }
+
+    return new THREE.Object3D();
+  }
+
+  setObjectTransform(object) {
+    const { type } = this.options;
+    switch (type) {
+      case POSE_VIZ_TYPES.arrow: {
+        const {
+          alpha,
+          shaftLength,
+          shaftRadius,
+          headLength,
+          headRadius
+        } = this.options;
+
+        object.setHead({ radius: headRadius, length: headLength });
+        object.setShaft({ radius: shaftRadius, length: shaftLength });
+        object.setAlpha(alpha);
+        break;
+      }
+      case POSE_VIZ_TYPES.axes: {
+        const { axesLength, axesRadius } = this.options;
+
+        object.setLength(axesLength);
+        object.setRadius(axesRadius);
+        break;
+      }
+    }
+  }
+
+  changeObjectPoolType() {
+    const tempObjectPool = [];
+
+    // remove prev type objects and push the new ones in place of them.
+    this.objectPool.forEach((object, index) => {
+      const { position, quaternion } = object;
+      object.parent.remove(object);
+      delete this.objectPool[index];
+
+      const newObj = this.getObject();
+      newObj.position.set(position.x, position.y, position.z);
+      newObj.quaternion.set(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
+      tempObjectPool.push(newObj);
+      this.object.add(newObj);
+      this.setObjectTransform(newObj);
+    });
+
+    this.objectPool = tempObjectPool;
+  }
+
+  updateOptions(options) {
+    const { type: currentType } = this.options;
+    const {
+      type,
+      positionTolerance,
+      angleTolerance,
+      keep,
+      alpha,
+      shaftLength,
+      shaftRadius,
+      headLength,
+      headRadius
+    } = options;
+
+    this.options = options;
+
+    if (type !== currentType) {
+      this.changeObjectPoolType();
+    }
+
+    this.objectPool.forEach((object) => {
+      this.setObjectTransform(object);
+    });
+
+    this.setKeepSize(keep);
   }
 
   update(message) {
@@ -100,9 +184,8 @@ class DisplayOdometry extends Core {
       return;
     }
 
-    const newObject = new Arrow();
-    newObject.setShaft({ length: SHAFT_LENGTH, radius: SHAFT_RADIUS });
-    newObject.setHead({ length: HEAD_LENGTH, radius: HEAD_RADIUS });
+    const newObject = this.getObject();
+    this.setObjectTransform(newObject);
 
     this.objectPool.push(newObject);
     this.currentObject += 1;
