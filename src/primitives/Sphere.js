c3161b48b24a93710361b95883c747b42ae225f7
Nishanth Mane <nishanth.mane@rapyuta-robotics.com>
2019-05-03 22:51:32 +0530

    Image data works for monoX other encodings are WIP

--- src/primitives/Sphere.js
+++ src/primitives/Sphere.js
@@ -6,7 +6,11 @@ const { THREE } = window;
 class Sphere extends Mesh {
   constructor(color, size = 1) {
     super();
-    this.geometry = new THREE.SphereGeometry(size, DEFAULT_RADIAL_SEGMENTS, DEFAULT_RADIAL_SEGMENTS);
+    this.geometry = new THREE.SphereGeometry(
+      size,
+      DEFAULT_RADIAL_SEGMENTS,
+      DEFAULT_RADIAL_SEGMENTS
+    );
     this.material = new THREE.MeshStandardMaterial({ color });
     this.rotateX(Math.PI / 2);
   }
--- src/viz/Image.js
+++ src/viz/Image.js
@@ -1,35 +1,95 @@
 import Core from '../core';
 import { MESSAGE_TYPE_IMAGE } from '../utils/constants';
 
+
 const { THREE } = window;
+
+const BASE64 =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+function decode64(x) {
+  const a = [];
+  let z = 0;
+  let bits = 0;
+
+  for (let i = 0, len = x.length; i < len; i++) {
+    z += BASE64.indexOf(x[i]);
+    bits += 6;
+    if (bits >= 8) {
+      bits -= 8;
+      a.push(z >> bits);
+      z &= ((2 ** bits) - 1);
+    }
+    z <<= 6;
+  }
+  return a;
+}
 
 class Image extends Core {
   constructor(ros, topicName, imageEle) {
     super(ros, topicName, MESSAGE_TYPE_IMAGE);
     this.object = new THREE.Group();
-    this.imageEle = imageEle;
   }
 
-  update(message) {
-    const { data } = message;
-    const { width, height } = message;
-    const ctx = this.imageEle.getContext('2d');
+  setImageRef(ref) {
+    this.imageCanvas = ref;
+  }
+
+  applyImageData(message) {
+    const { data, step } = message;
+    const { width, height, is_bigendian: isBigEndian } = message;
+
+    const ctx = this.imageCanvas.getContext('2d');
     const imgData = ctx.createImageData(width, height);
-    const encodeToUInt8 = new TextEncoder().encode(data);
+    const { encoding } = message;
 
-    // depends on the data being sent
-    // set to 3 since considering for bgr8 formal only
-    const offset = 3;
+    const newData = [];
+    const decodedData = atob(data);
+    decodedData.split('').forEach((data, index) => {
+      newData.push(decodedData.charCodeAt(index));
+    });
 
-    for (let i = 0; i < encodeToUInt8.length; i += offset) {
-      // Since the encode data is in the bgr order
-      imgData.data[i + 0] = encodeToUInt8[i + 2];
-      imgData.data[i + 1] = encodeToUInt8[i + 1];
-      imgData.data[i + 2] = encodeToUInt8[i + 0];
-      imgData.data[i + 3] = 255;
+    const encodeToUInt8 = Uint8Array.from(newData);
+    const encodedDataView = new DataView(encodeToUInt8.buffer);
+    console.log(encodedDataView);
+
+    switch (encoding) {
+      case 'mono8': {
+        let j = 0;
+        for (let i = 0; i < step * height; i++) {
+          imgData.data[j++] = encodedDataView.getUint8(i, !isBigEndian);
+          imgData.data[j++] = encodedDataView.getUint8(i, !isBigEndian);
+          imgData.data[j++] = encodedDataView.getUint8(i, !isBigEndian);
+          imgData.data[j++] = 255;
+        }
+        break;
+      }
+      case 'bgr8': {
+        const offset = 3;
+
+        for (let i = 0; i <  step * height; i += offset) {
+          imgData.data[i + 0] = encodedDataView.getUint8(i + 2, !isBigEndian);
+          imgData.data[i + 1] = encodedDataView.getUint8(i + 0, !isBigEndian);
+          imgData.data[i + 2] = encodedDataView.getUint8(i + 1, !isBigEndian);
+          imgData.data[i + 3] = 255;
+        }
+        break;
+      }
+      default:
+        break;
     }
 
     ctx.putImageData(imgData, 0, 0);
+  }
+
+  update(message) {
+    if (this.imageCanvas) {
+      const { data } = message;
+      const { width, height } = message;
+
+      this.imageCanvas.width = width;
+      this.imageCanvas.height = height;
+
+      this.applyImageData(message);
+    }
   }
 }
 
