15f29e1998beac7b47f60c35f31b4617e6e7d566
Ashish Chaudhary <iamashishchaudhary@gmail.com>
2019-09-13 11:57:37 +0530

    fix eslint errors

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 
 import {
-  OBJECT_TYPE_ARROW, 
-  OBJECT_TYPE_AXES, 
-  OBJECT_TYPE_FLAT_ARROW, 
+  OBJECT_TYPE_ARROW,
+  OBJECT_TYPE_AXES,
+  OBJECT_TYPE_FLAT_ARROW,
   OBJECT_TYPE_ARROW_WITH_CIRCLE,
 } from './constants';
 
@@ -56,28 +56,38 @@ export const setObjectDimension = (object, options) => {
     }
     case OBJECT_TYPE_ARROW_WITH_CIRCLE:
       const {
-              color,
-              alpha,
-              arc,
-              circleConeRadius,
-              circleConeLength,
-              circleRadius,
-              headLength,
-              headRadius,
-              radialSegments,
-              shaftLength,
-              shaftRadius,
-              tube,
-              tubularSegments
-            } = options;
+        alpha,
+        circleConeLength,
+        circleConeRadius,
+        circleRadius,
+        color,
+        headLength,
+        headRadius,
+        shaftLength,
+        shaftRadius,
+        tube,
+      } = options;
 
-            object.setHeadDimensions({ radius: headRadius, length: headLength });
-            object.setShaftDimensions({ radius: shaftRadius, length: shaftLength });
-            object.setTorusDimensions({radius: circleRadius, tube: tube});
-            object.setCircleConeDimensions({radius: circleConeRadius, length: circleConeLength});
-            object.setAlpha({cone: alpha, cylinder: alpha, torus: alpha, circleCone: alpha});
-            object.setColor({ cone: new THREE.Color(color), cylinder: new THREE.Color(color), torus: new THREE.Color(color), circleCone: new THREE.Color(color)});
-            break;
+      object.setHeadDimensions({ radius: headRadius, length: headLength });
+      object.setShaftDimensions({ radius: shaftRadius, length: shaftLength });
+      object.setTorusDimensions({ radius: circleRadius, tube });
+      object.setCircleConeDimensions({
+        radius: circleConeRadius,
+        length: circleConeLength,
+      });
+      object.setAlpha({
+        cone: alpha,
+        cylinder: alpha,
+        torus: alpha,
+        circleCone: alpha,
+      });
+      object.setColor({
+        cone: new THREE.Color(color),
+        cylinder: new THREE.Color(color),
+        torus: new THREE.Color(color),
+        circleCone: new THREE.Color(color),
+      });
+      break;
   }
 };
 
--- src/viz/Wrench.js
+++ src/viz/Wrench.js
@@ -8,8 +8,6 @@ import {
 } from '../utils/constants';
 import Arrow from '../primitives/Arrow';
 import ArrowWithCircle from '../primitives/ArrowWithCircle';
-import Axes from '../primitives/Axes';
-import LineArrow from '../primitives/LineArrow';
 import { setObjectDimension } from '../utils/helpers';
 
 class Wrench extends Core {
@@ -25,9 +23,7 @@ class Wrench extends Core {
   }
 
   static getNewPrimitive(options) {
-    const {
-      type
-    } = options;
+    const { type } = options;
     let newObject = null;
 
     switch (type) {
@@ -42,12 +38,12 @@ class Wrench extends Core {
   }
 
   updatePrimitive(primitive) {
-    if(primitive && (primitive.type !== this.options.type)) {
+    if (primitive && primitive.type !== this.options.type) {
       this.object.remove(primitive);
       primitive = null;
     }
 
-    if(!primitive) {
+    if (!primitive) {
       primitive = Wrench.getNewPrimitive(this.options);
       this.object.add(primitive);
     }
@@ -62,51 +58,67 @@ class Wrench extends Core {
     this.options.type = WRENCH_OBJECT_TYPES.arrowWithCircle;
     this.primitive2 = this.updatePrimitive(this.primitive2);
 
-    var forceOptions = {
-      color: this.options["forceColor"],
-      alpha: this.options["alpha"],
-      headLength: this.options["headLength"]*this.options["forceArrowScale"],
-      headRadius: this.options["headRadius"]*this.options["arrowWidth"],
-      shaftLength: this.options["shaftLength"]*this.options["forceArrowScale"],
-      shaftRadius: this.options["shaftRadius"]*this.options["arrowWidth"],
-      type: WRENCH_OBJECT_TYPES.arrow
+    const forceOptions = {
+      color: this.options.forceColor,
+      alpha: this.options.alpha,
+      headLength: this.options.headLength * this.options.forceArrowScale,
+      headRadius: this.options.headRadius * this.options.arrowWidth,
+      shaftLength: this.options.shaftLength * this.options.forceArrowScale,
+      shaftRadius: this.options.shaftRadius * this.options.arrowWidth,
+      type: WRENCH_OBJECT_TYPES.arrow,
     };
 
-    var torqueOptions = {
-      color: this.options["torqueColor"],
-      alpha: this.options["alpha"],
-      arc: this.options["arc"],
-      circleConeRadius: this.options["circleConeRadius"]*this.options["arrowWidth"],
-      circleConeLength: this.options["circleConeLength"]*this.options["arrowWidth"],
-      circleRadius: this.options["circleRadius"]*this.options["torqueArrowScale"],
-      headLength: this.options["headLength"]*this.options["torqueArrowScale"],
-      headRadius: this.options["headRadius"]*this.options["arrowWidth"],
-      shaftLength: this.options["shaftLength"]*this.options["torqueArrowScale"],
-      shaftRadius: this.options["shaftRadius"]*this.options["arrowWidth"],
-      tube: this.options["tube"] *this.options["arrowWidth"],
-      tubularSegments: this.options["tubularSegments"],
-      type: WRENCH_OBJECT_TYPES.arrowWithCircle
+    const torqueOptions = {
+      color: this.options.torqueColor,
+      alpha: this.options.alpha,
+      arc: this.options.arc,
+      circleConeRadius: this.options.circleConeRadius * this.options.arrowWidth,
+      circleConeLength: this.options.circleConeLength * this.options.arrowWidth,
+      circleRadius: this.options.circleRadius * this.options.torqueArrowScale,
+      headLength: this.options.headLength * this.options.torqueArrowScale,
+      headRadius: this.options.headRadius * this.options.arrowWidth,
+      shaftLength: this.options.shaftLength * this.options.torqueArrowScale,
+      shaftRadius: this.options.shaftRadius * this.options.arrowWidth,
+      tube: this.options.tube * this.options.arrowWidth,
+      tubularSegments: this.options.tubularSegments,
+      type: WRENCH_OBJECT_TYPES.arrowWithCircle,
     };
-    
+
     setObjectDimension(this.primitive1, forceOptions);
     setObjectDimension(this.primitive2, torqueOptions);
   }
 
   update(message) {
     super.update(message);
-    const { wrench: { force, torque } } = message;
-    var translationVector = new THREE.Vector3(1, 0, 0);
-    var forceVector = new THREE.Vector3(force.x, force.y, force.z).normalize();
-    var torqueVector = new THREE.Vector3(torque.x, torque.y, torque.z).normalize();
-    var forceQuaternion = new THREE.Quaternion().setFromUnitVectors(translationVector, forceVector);
-    var torqueQuaternion = new THREE.Quaternion().setFromUnitVectors(translationVector, torqueVector);
+    const {
+      wrench: { force, torque },
+    } = message;
+    const translationVector = new THREE.Vector3(1, 0, 0);
+    const forceVector = new THREE.Vector3(
+      force.x,
+      force.y,
+      force.z,
+    ).normalize();
+    const torqueVector = new THREE.Vector3(
+      torque.x,
+      torque.y,
+      torque.z,
+    ).normalize();
+    const forceQuaternion = new THREE.Quaternion().setFromUnitVectors(
+      translationVector,
+      forceVector,
+    );
+    const torqueQuaternion = new THREE.Quaternion().setFromUnitVectors(
+      translationVector,
+      torqueVector,
+    );
     this.primitive1.setTransform({
-      translation: {y: 0, x: 0, z: 0},
-      rotation: forceQuaternion
+      translation: { y: 0, x: 0, z: 0 },
+      rotation: forceQuaternion,
     });
     this.primitive2.setTransform({
-      translation: {y: 0, x: 0, z: 0},
-      rotation: torqueQuaternion
+      translation: { y: 0, x: 0, z: 0 },
+      rotation: torqueQuaternion,
     });
   }
 }
