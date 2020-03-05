605d6c8b72f28c4af102e7231b856eb303108193
Himanshu <himanshu.chhabra@rapyuta-robotics.com>
2020-03-05 12:43:41 +0530

    Moving 2d/3d viewer up to viewer class.

--- src/viewers/viewer.ts
+++ src/viewers/viewer.ts
@@ -1,8 +1,16 @@
-import { Object3D, Vector3, WebGLRenderer } from 'three';
+import {
+  Object3D,
+  Vector3,
+  WebGLRenderer,
+  OrthographicCamera,
+  PerspectiveCamera,
+} from 'three';
 import { ResizeObserver as ResizeObserverPolyfill } from '@juggle/resize-observer';
 
+import { EditorControls } from '../utils/editorControls';
+import { MapControls2D } from '../utils/2dControls';
 import Scene from '../core/scene';
-import { DEFAULT_OPTIONS_SCENE } from '../utils/constants';
+import { DEFAULT_OPTIONS_SCENE, VIEW_TYPES } from '../utils/constants';
 import { assertIsDefined } from '../utils/helpers';
 
 const ResizeObserver = (window as any).ResizeObserver || ResizeObserverPolyfill;
@@ -14,9 +22,11 @@ class Viewer {
   public previousHeight = 0;
   public ro: typeof ResizeObserver;
   public renderer?: WebGLRenderer;
-  public camera: any;
+  public camera?: OrthographicCamera | PerspectiveCamera;
   public container?: HTMLElement;
+  // @ts-ignore
   public controls?: any;
+  public viewType: any;
 
   constructor(scene: Scene | null, options: object = {}) {
     this.options = {
@@ -26,9 +36,12 @@ class Viewer {
     this.scene =
       scene || new Scene(this.options as typeof DEFAULT_OPTIONS_SCENE);
 
+    this.initCamera();
+
     this.animate = this.animate.bind(this);
     this.onWindowResize = this.onWindowResize.bind(this);
     this.onResizeCb = this.onResizeCb.bind(this);
+    this.switchView = this.switchView.bind(this);
   }
 
   initObserver() {
@@ -39,8 +52,6 @@ class Viewer {
     });
   }
 
-  onResizeCb() {}
-
   animate() {
     this.scene.stats.begin();
     this.scene.updateMatrixWorld();
@@ -50,19 +61,79 @@ class Viewer {
     requestAnimationFrame(this.animate);
   }
 
-  protected initCamera(camera: any) {
+  switchView(newViewType: number) {
+    const { viewType } = this.options as typeof DEFAULT_OPTIONS_SCENE;
+
+    if (newViewType === viewType) {
+      return;
+    }
+
+    switch (newViewType) {
+      case VIEW_TYPES.VIEW_2D: {
+        this.camera = new OrthographicCamera(-100, 100, 100, -100, 0.1, 1000);
+        this.camera.zoom = 0.5;
+        this.camera.position.set(0, 0, 10);
+        // @ts-ignore
+        this.controls = new MapControls2D(this.camera, this.container);
+        break;
+      }
+      case VIEW_TYPES.VIEW_3D:
+      default: {
+        this.camera = new PerspectiveCamera(50, 1, 0.01, 1000);
+        this.camera.position.set(0, 5, 10);
+        // @ts-ignore
+        this.controls = new EditorControls(this.camera, this.container);
+        break;
+      }
+    }
+  }
+
+  protected initCamera() {
+    const { viewType } = this.options as typeof DEFAULT_OPTIONS_SCENE;
+
+    let camera: OrthographicCamera | PerspectiveCamera | null = null;
+
+    switch (viewType) {
+      case VIEW_TYPES.VIEW_2D: {
+        camera = new OrthographicCamera(-100, 100, 100, -100, 0.1, 1000);
+        camera.zoom = 0.5;
+        camera.position.set(0, 0, 10);
+        break;
+      }
+      case VIEW_TYPES.VIEW_3D:
+      default: {
+        camera = new PerspectiveCamera(50, 1, 0.01, 1000);
+        camera.position.set(0, 5, 10);
+      }
+    }
+
+    camera.up = new Vector3(0, 0, 1);
+    camera.lookAt(new Vector3());
     this.camera = camera;
-    this.camera.up = new Vector3(0, 0, 1);
-    this.camera.lookAt(new Vector3());
 
     this.scene.add(this.camera);
   }
 
-  setContainer(domNode: HTMLElement, controls: any) {
+  setContainer(domNode: HTMLElement) {
+    const { viewType } = this.options as typeof DEFAULT_OPTIONS_SCENE;
+
+    switch (viewType) {
+      case VIEW_TYPES.VIEW_2D: {
+        // @ts-ignore
+        this.controls = new MapControls2D(this.camera, this.container);
+        break;
+      }
+      case VIEW_TYPES.VIEW_3D:
+      default: {
+        // @ts-ignore
+        this.controls = new EditorControls(this.camera, this.container);
+        break;
+      }
+    }
+
     this.container = domNode;
     this.initRenderer(domNode);
-    // @ts-ignore
-    this.controls = controls;
+
     this.controls.enableDamping = true;
     this.ro.observe(this.container);
     requestAnimationFrame(this.animate);
@@ -89,6 +160,31 @@ class Viewer {
 
   destroy() {
     this.ro.unobserve(this.container!);
+  }
+
+  onResizeCb() {
+    const { viewType } = this.options as typeof DEFAULT_OPTIONS_SCENE;
+
+    switch (viewType) {
+      case VIEW_TYPES.VIEW_2D: {
+        const { offsetWidth, offsetHeight } = this.container as HTMLElement;
+        const [cameraWidth, cameraHeight] = [
+          offsetWidth / 100,
+          offsetHeight / 100,
+        ];
+        this.camera.left = -cameraWidth / 2;
+        this.camera.right = cameraWidth / 2;
+        this.camera.top = cameraHeight / 2;
+        this.camera.bottom = -cameraHeight / 2;
+        break;
+      }
+      case VIEW_TYPES.VIEW_3D:
+      default: {
+        const { offsetHeight, offsetWidth } = this.container as HTMLElement;
+        this.camera.aspect = offsetWidth / offsetHeight;
+        break;
+      }
+    }
   }
 
   onWindowResize() {
